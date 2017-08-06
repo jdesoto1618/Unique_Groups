937645078b88a6168bde9c0106ce565393a1c178
Joe DeSoto <modulusman@gmail.com>
2017-08-06 12:06:31 -0700

    Update Group_Randomizer.js
    Added initial try to bottom

--- Group_Randomizer.js
+++ Group_Randomizer.js
@@ -12,3 +12,19 @@ while ( num_people > 1 ) { //As long as there are still two people to be paired(
    });
     console.log(rst[ 1 ] + " and " + rst[ 2 ] + " are a group!");
 }
+
+//Previous attempt//
+// Range is 0 to 10, since using floor the program doesn't reach 10. Thus the range is 0 to 9, ten total possibilites
+// var used = [];
+// for (i = 0; i < 5; i++) { // Run this six times to generate six groups
+//   var pick1 = Math.floor(Math.random()*10);
+//   var pick2 = Math.floor(Math.random()*10);
+//
+// Change a number if they are the same
+//   while (pick1 == pick2) {
+//     pick2 = Math.floor(Math.random()*10);
+//   }
+//   used.push(people[pick1] + " " + people[pick2]);
+// console.log(people[pick1] + " and " + people[pick2] + " are a group!");
+// } //ends for loop
+// console.log(used);
