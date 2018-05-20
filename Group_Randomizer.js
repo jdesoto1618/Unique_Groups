f10dbb1517c5dfc096ef36a5096ec4e6e085f4f8
Joe DeSoto <modulusman@gmail.com>
2018-05-19 18:38:59 -0700

    updates commenting

--- Group_Randomizer.js
+++ Group_Randomizer.js
@@ -1,16 +1,16 @@
 var people = ["Joe", "Amy", "Garrett", "Mark", "Matt", "Sandro", "Sharmila", "Rob", "Bri", "Ritzy"];
-var num_people = people.length; //Declare the length and store it
-rst = {}; //Initialize an empty object. This will contain the unique pairs
+var num_people = people.length; // save length
+rst = {}; // initialize an empty object. This will contain the unique pairs
 
-while ( num_people > 1 ) { //As long as there are still two people to be paired(since people gets spliced)
-    [1,2].forEach( function( num ) { //Place two people [1,2] next to each other in the new object
-       var index =  Math.floor(Math.random() * num_people);
+while (num_people > 1) { // as long as there are still two people to be paired(since people gets spliced)
+    [1,2].forEach( function(num) { // place two people [1,2] next to each other in the new object
+       var index =  Math.floor(Math.random() * num_people); // choose a random index based on the number of people
 	   console.log(num);
-       rst[ num ] = people[ index ];
-       people.splice( index, 1 );
-       num_people--;
+       rst[num] = people[index];
+       people.splice(index, 1);
+       num_people--; // decrement the number of people left from which to create a unique group
    });
-    console.log(rst[ 1 ] + " and " + rst[ 2 ] + " are a group!");
+    console.log(rst[1] + " and " + rst[2] + " are a group!");
 }
 
 //Previous attempt//
